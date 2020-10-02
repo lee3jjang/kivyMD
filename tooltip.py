f4b799ea64ff8a0436419522e3faad6e57ec72d3
unknown <lee3jjang@gmail.com>
2020-10-03 02:41:42 +0900

    12121

---
+++ tooltip.py
@@ -0,0 +1,19 @@
+from kivy.lang import Builder
+from kivymd.app import MDApp
+
+KV = '''
+<TooltipMDIconButton@MDIconButton+MDTooltip>
+
+Screen:
+    TooltipMDIconButton:
+        icon: "language-python"
+        tooltip_text: "Hello Tooltip!"
+        pos_hint: {'center_x': .5, 'center_y': .5}
+'''
+
+class Test(MDApp):
+    def build(self):
+        return Builder.load_string(KV)
+
+
+Test().run()
