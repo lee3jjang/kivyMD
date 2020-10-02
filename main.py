2863be7a6970a0676cd579a1f71101e46d855431
unknown <lee3jjang@gmail.com>
2020-10-02 23:35:32 +0900

    12121

--- main.py
+++ main.py
@@ -1,10 +1,41 @@
 import os
 from kivymd.app import MDApp
 from kivymd.uix.label import MDLabel
+from kivymd.uix.button import MDRectangleFlatButton
+from kivy.uix.screenmanager import Screen
+from kivy.lang import Builder
+
+KV = '''
+Screen:
+    # app = MainApp()
+    MDRaisedButton:
+        text: "primary_light"
+        pos_hint: {"center_x": 0.5, "center_y": 0.7}
+        md_bg_color: app.theme_cls.primary_light
+
+    MDRaisedButton:
+        text: "primary_color"
+        pos_hint: {"center_x": 0.5, "center_y": 0.5}
+
+    MDRaisedButton:
+        text: "primary_dark"
+        pos_hint: {"center_x": 0.5, "center_y": 0.3}
+        md_bg_color: app.theme_cls.primary_dark
+
+'''
 
 class MainApp(MDApp):
     title = 'My Main Title'
+    
     def build(self):
-        return MDLabel(text="Hello", halign="center")
+        # primary_palette, primary_hue
+        self.theme_cls.primary_palette = 'Purple'
+        self.theme_cls.primary_hue = "500"
+        self.theme_cls.theme_style = "Light"
+        
 
-MainApp().run()
+        return Builder.load_string(KV)
+
+
+app = MainApp()
+app.run()
