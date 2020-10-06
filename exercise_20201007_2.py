6c0244262c7eb66abe91f26458014e406cf6d01b
unknown <lee3jjang@gmail.com>
2020-10-07 00:44:52 +0900

    12121

--- exercise_20201007_2.py
+++ exercise_20201007_2.py
@@ -1,5 +1,7 @@
 from kivy.lang import Builder
 from kivymd.app import MDApp
+from kivy.core.window import Window
+Window.size = (300, 500)
 
 KV = '''
 #:import MDListBottomSheet kivymd.uix.bottomsheet.MDListBottomSheet
@@ -9,21 +11,42 @@ KV = '''
 Screen:
     name: "MainScreen"
 
-    MDToolbar:
-        title: "Example"
-        pos_hint: {"top": 1}
-        elevation: 10
+    BoxLayout:
+        orientation: "vertical"
+        
+        MDToolbar:
+            title: "Example"
+            pos_hint: {"top": 1}
+            left_action_items: [["menu", lambda x: print(x)]]
+            elevation: 10
 
-    MDRaisedButton:
-        text: "Open list bottom sheet"
-        on_release:
-            bottom_sheet_menu = MDGridBottomSheet()
-            bottom_sheet_menu.add_item("Standard Item", lambda x: toast("youtube"), icon_src="youtube")
-            bottom_sheet_menu.open()
-            print(self)
-            print(app)
-            print(root)
-        pos_hint: {"center_x": .5, "center_y": .5}
+        MDTabs:
+            id: tabs
+
+            MDTabsBase:
+                text: "Tab 1"
+            MDTabsBase:
+                text: "Tab 2"
+            MDTabsBase:
+                text: "Tab 3"
+            MDTabsBase:
+                text: "Tab 4"
+            MDTabsBase:
+                text: "Tab 5"
+
+        MDRaisedButton:
+            text: "Open list bottom sheet"
+            on_release:
+                bottom_sheet_menu = MDGridBottomSheet()
+                bottom_sheet_menu.add_item("Standard Item", lambda x: toast("youtube"), icon_src="youtube")
+                bottom_sheet_menu.open()
+                print(self)
+                print(app)
+                print(root)
+            pos_hint: {"center_x": .5, "center_y": .5}
+
+        MDTextField:
+            hint_text: "This is hint text"
 '''
 
 class Test(MDApp):
