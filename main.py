26d8d1246ba996fd57b792a31df1b85cc99f7367
unknown <lee3jjang@gmail.com>
2020-10-10 12:08:29 +0900

    111111

--- main.py
+++ main.py
@@ -1,21 +1,11 @@
-# from kivy.core.window import Window
+from kivy.core.window import Window
 from kivy.lang import Builder
 from kivymd.app import MDApp
-import json
+Window.size = (144*3, 256*3)
 
-# class MyApp(MDApp):
-#     def build(self):
-#         return Builder.load_file('libs/kv/start_screen.kv')
+class MyApp(MDApp):
+    def build(self):
+        return Builder.load_file('libs/kv/start_screen.kv')
 
-# MyApp().run()
+MyApp().run()
 
-with open('screens_data.json') as json_file:
-    data_screens = json.load(json_file)
-for name_item_example in data_screens:
-    data = {
-        'text': name_item_example,
-        'icon': data_screens[name_item_example]['icon'],
-    }
-
-    print(data['icon'])
-    
