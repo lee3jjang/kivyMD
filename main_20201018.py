bbb23ec642ae277a90ff0a6bae4efa20b00f7a2d
unknown <lee3jjang@gmail.com>
2020-10-18 02:02:34 +0900

    11

--- main_20201018.py
+++ main_20201018.py
@@ -3,6 +3,7 @@ from kivy.lang import Builder
 from kivymd.app import MDApp
 from kivy.factory import Factory
 from kivy.uix.screenmanager import Screen
+from kivymd.uix.menu import MDDropdownMenu
 Window.size = (144*3, 256*3)
 
 KV = '''
@@ -13,7 +14,9 @@ KV = '''
         orientation: 'vertical'
 
         MDToolbar:
+            id: drop_item
             title: 'MDLabel'
+            right_action_items: [['menu', lambda x: root.open_menu(x)]]
 
         MDLabel:
             id: label
@@ -40,6 +43,24 @@ Main:
 '''
 
 class Main(Screen):
+    menu = None
+
+    def open_menu(self, instance_button):
+        menu_items = [{"icon": "git", "text": f"Item {i}"} for i in range(5)]
+        if self.menu == None:
+            self.menu = MDDropdownMenu(
+                caller=instance_button,
+                items=menu_items,
+                position="auto",
+                width_mult=4,
+            )
+        self.menu.bind(on_release=self.set_item)
+        self.menu.open()
+
+    def set_item(self, instance_menu, instance_menu_item):
+        self.ids.label.text = instance_menu_item.text
+        self.menu.dismiss()
+
     def slider_move(self, instance_slider, mouse_motion):
         self.ids.label.text = str(instance_slider.value/100)
         instance_slider.size_hint_y = instance_slider.value/100
