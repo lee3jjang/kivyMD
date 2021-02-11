7ad21ab21819a0b34b2824d1b9480415dbda97f9
Lee Sang-Jin <lee3jjang@gmail.com>
2021-02-11 16:15:28 +0900

    12

--- libs/baseclass/list_items.py
+++
@@ -1,5 +0,0 @@
-from kivy.properties import StringProperty
-from kivymd.uix.list import OneLineAvatarListItem
-
-class KitchenSinkOneLineLeftIconItem(OneLineAvatarListItem):
-    icon = StringProperty()
