0bb263a3f82150c6b8ff8b3832a065c320f98190
Lee Sang-Jin <lee3jjang@gmail.com>
2021-02-11 16:34:04 +0900

    121212

---
+++ docs/source/index.rst
@@ -0,0 +1,14 @@
+이 프로젝트는 KivyMD 연습을 위해 만들어졌습니다.
+
+KivyMD 연습
+==================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
---
+++ kivymd_exercise/main.py
@@ -0,0 +1,12 @@
+"""
+MDCrane demo
+============
+
+.. seealso:
+
+    `Material Design spec,
+    Crane <https://material.io/design/material-studies/crane.html#>`
+    
+    Crane is a travel app that helps users find and book travel, lodging, and
+    restaurant options that match their input preferences.
+"""
