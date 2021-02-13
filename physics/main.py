dab871b05ddf5c1748a109e6e1132d49926d9b02
sangjin <lee3jjang@gmail.com>
2021-02-13 10:16:22 +0900

    연습

---
+++ physics/main.py
@@ -0,0 +1,33 @@
+import os
+from pathlib import Path
+from kivy.app import App
+
+from kivy.lang import Builder
+from kivy.uix.label import Label
+from kivy.uix.scatter import Scatter
+from kivy.uix.textinput import TextInput
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
+
+os.environ["PHYSICS_ROOT"] = str(Path(__file__).parent)
+KV_DIR = f"{os.environ['PHYSICS_ROOT']}/libs/kv/"
+
+for kv_file in os.listdir(KV_DIR):
+    with open(os.path.join(KV_DIR, kv_file), encoding="utf-8") as kv:
+        Builder.load_string(kv.read())
+
+KV = """
+#:import ScatterTextWidget libs.baseclass.root_screen.ScatterTextWidget
+
+ScatterTextWidget:
+"""
+
+class TutorialApp(App):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.title = "Physics"
+
+    def build(self):
+        return Builder.load_string(KV)
+        
+TutorialApp().run()
